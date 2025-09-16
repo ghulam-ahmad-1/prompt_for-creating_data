84c180fe142366033d127c4e15e03051554a0fdd
ghulam-ahmad-1 <2021ee305@student.uet.edu.pk>
2025-09-16 22:50:13 +0500

    adding data

---
+++ extraction_labels.py
@@ -0,0 +1,49 @@
+extraction_labels = {
+
+    "birth_certificate": [
+        "dob",
+        "father_name",
+        "gender",
+        "issue_date",
+        "mother_name",
+        "name",
+        "Birth_place",
+        "registration_number",
+    ],
+
+    "id_card": [
+        "address",
+        "cnic_number",
+        "dob",
+        "expiry_date",
+        "name_of_father",
+        "gender",
+        "issue_date",
+        "name",
+        "name_of_mother",
+        "name_of_spouse"
+    ],
+
+    "license": [
+        "address",
+        "blood_group",
+        "dob",
+        "expiry_date",
+        "issue_date",
+        "license_number",
+        "name",
+        "vehicle_class",
+    ],
+
+    "passport": [
+        "dob",
+        "expiry_date",
+        "gender",
+        "issue_date",
+        "name",
+        "nationality",
+        "passport_number",
+        "place_of_birth",
+    ],
+
+}
